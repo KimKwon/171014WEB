1110f5c576af8f3015145b195e06a791f8ddcf60
Darkeroe <darkeroe@hanyang.ac.kr>
2017-11-20 18:36:49 +0900

    add sample

--- database/database.sql
+++ database/database.sql
@@ -10,14 +10,14 @@ create table user_info(
 
 create table room(
 	room_no integer not null,
-	room_date varchar(20) not null,
+	room_date timestamp not null,
 	room_check varchar(10) not null
 );
 alter table room add primary key(room_no, room_date);
 
 create table reservation(
 	id varchar(30) not null,
-	reserve_date varchar(20) not null,
+	reserve_date timestamp not null,
 	reserve_room_no integer not null,
 	purpose varchar(300),
 	population integer not null,
@@ -26,4 +26,14 @@ create table reservation(
 
 alter table reservation add primary key(reserve_date, reserve_room_no);
 
-alter table reservation add foreign key(reserve_date) references user_info(user_id);
+alter table reservation add foreign key(id) references user_info(user_id);
+alter table reservation add foreign key(reserve_date) references room(room_date) on delete cascade;
+
+insert into user_info values('A', 'asd123', 'A@naver.com');
+insert into user_info values('B', 's2d4f5e', 'B@naver.com');
+insert into user_info values('C', '2', 'C@naver.com');
+insert into room values(1, '2017-10-10 11:10:00', 'FALSE');
+insert into room values(2, '2010-01-01 01:01:00', 'TRUE');
+insert into room values(3, '2017-10-10 13:00:00', 'FALSE');
+insert into reserve values('A', '2017-10-10 11:10:00', 1,'test', 6);
+insert into reserve values('B', '2010-01-01 01:01:00', 2,'tesdst', 3);
