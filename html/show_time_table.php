8bcbd65076573250c96ff23e5dbe904d7a9e371a
baesh3744 <baesh3744@naver.com>
2017-12-01 21:47:14 +0900

    Create show_time_table.php

---
+++ html/show_time_table.php
@@ -0,0 +1,27 @@
+<?php
+try {
+    $room_no = $_GET["room_number"];
+    $query = "SELECT reserve_time FROM reservation WHERE reserve_room_no = $room_no";
+    $db = new PDO("mysql:dbname=smash", "root", "root");
+    $rows = $db->query($query);
+    $time_table = array();
+    for($i = 0; $i < 20; $i++) {
+        $time_table[$i] = "possible";
+    }
+    foreach($rows as $row) {
+        $time_table[$row["reserve_time"]] = "impossible";
+    }
+
+    header("Content-type: application/json");
+    print "{\n  \"times\": [\n";
+    for($i = 9; $i < 20; $i++) {
+        if($i != 9) { print ",\n"; }
+        $status = $time_table[$i];
+        print "{\"time\": $i, \"status\": \"$status\"}";
+        if($i == 19) { print "\n"; }
+    }
+    print "  ]\n}\n";
+} catch (PDOException $e) {
+    echo "fail";
+}
+?>
