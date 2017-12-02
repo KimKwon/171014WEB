02e3e6db602a982750def29e2fa79bdc990ad29f
gunhee8178 <gunhee8178@naver.com>
2017-12-03 01:04:40 +0900

    Change color
    When you choose room

--- html/show_time_table.php
+++ html/show_time_table.php
@@ -1,5 +1,20 @@
 <?php
+
+$delay = 0;
+
+
+
 try {
+
+
+    if (isset($_REQUEST["delay"])) {
+    	$delay = max(0, min(60, (int) filter_chars($_REQUEST["delay"])));
+    }
+
+    if ($delay > 0) {
+    	sleep($delay);
+    }
+
     $room_no = $_GET["room_number"];
     $query = "SELECT reserve_time FROM reservation WHERE reserve_room_no = $room_no";
     $db = new PDO("mysql:dbname=smash", "root", "root");
