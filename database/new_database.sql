a1b44f39d59874e61f116856dc5b33e829db8cac
Darkeroe <darkeroe@hanyang.ac.kr>
2017-12-15 01:58:01 +0900

    database
    테이블 2개로 하고 reserve_table은 사용 시간

--- database/new_database.sql
+++ database/new_database.sql
@@ -13,6 +13,7 @@ create table reservation(
 	reserve_date date,
 	reserve_time integer,
 	reserve_room_no integer not null,
+	reserve_period integer not null,
 	purpose varchar(300),
 	population integer not null
 );
@@ -20,3 +21,10 @@ create table reservation(
 alter table reservation add primary key(reserve_date, reserve_room_no);
 
 alter table reservation add foreign key(id) references user_info(user_id);
+
+insert into user_info values('A', 'asd123', 'A@naver.com');
+insert into user_info values('B', 's2d4f5e', 'B@naver.com');
+insert into user_info values('C', '2', 'C@naver.com');
+insert into reservation values('A','17/12/15', 12, 1, 1, 'Study', 6);
+insert into reservation values('A','17/12/15', 19, 1, 2, 'Study', 5);
+insert into reservation values('B','17/12/13', 15, 2, 3, 'Study', 6);
