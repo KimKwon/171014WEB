92374f9db7bb4922d53d7a3dbebceb54c04300d1
KimKwon <khj9709@icloud.com>
2017-12-17 22:17:45 +0900

    error fix

--- html/get_reserve_info.php
+++ html/get_reserve_info.php
@@ -4,9 +4,9 @@ $time = $_POST['room_time'];
 $date = $_POST['date'];
 
 
-
-$db = new PDO("mysql:dbname=smash;host:localhost","root","root");		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-$query = "SELECT * FROM reservation NATURAL JOIN user_info WHERE (reserve_date='2017-12-17' AND reserve_time=13 AND reserve_room_no=1);";
+$db = new PDO("mysql:host=gs-db-instance1.cgkevqnkktap.ap-northeast-2.rds.amazonaws.com;port=3306;dbname=smash","smash","smash1219");		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+// $db = new PDO("mysql:dbname=smash;host:localhost","root","root");		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$query = "SELECT * FROM reservation NATURAL JOIN user_info WHERE (reserve_date='$date' AND reserve_time=$time AND reserve_room_no=$num);";
 $rows = $db->query($query);
 $a = empty($rows);
 foreach ($rows as $row ) {
