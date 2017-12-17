3433756c5ab3ef00ea9ed7f561b49bba02de0b0e
yungoingFLY <yun_going@naver.com>
2017-12-17 21:48:24 +0900

    ver3.4

--- html/mypage.php
+++ html/mypage.php
@@ -52,41 +52,45 @@
                         <h2>#<?= $user_id ?>님의 My Page</h2>
                         <div class="grid">
                         <ul id="Grid">
-                            <li>학생정보
-                                <div class = "infor">
-                                    <?php foreach($rows as $row) { ?>
-                                        <p>이름: <?= $row["user_id"] ?></p>
-                                        <?php if($row["student_id"] == null) {?> <p>등록된 학번이 없습니다.</p>
-                                        <?php } else {?> <p>학번: <?= $row["student_id"] ?></p> <?php } ?>
-                                        <?php if($row["student_id"] == null) {?> <p>등록된 학과가 없습니다.</p>
-                                        <?php } else {?> <p>학과: <?= $row["department"] ?></p> <?php } ?>
-                                    <?php } ?>
-                                </div>
-                                <div class="bt">
-                                    <a href=""><button>회원정보수정</button></a>
-                                <div>
-                            </li>
-                            <li>예약확인
-                                <div class = "infor">
-                                    <?php $query = "SELECT * FROM reservation WHERE id='$user_id'";
-                                        $rows = $db->query($query);
-                                        if($rows->rowCount() == 0) {
-                                    ?>
-                                        <p>예약이 없습니다.</p>
-                                    <?php
-                                        } else {
-                                            foreach($rows as $row) {
-                                    ?>
-                                        <p>방번호: <?= $row["reserve_room_no"] ?></p>
-                                        <p>예약날짜: <?= $row["reserve_date"] ?></p>
-                                        <p>예약시간: <?= $row["reserve_time"] ?></p>
-                                    <?php   } ?>
-                                    <?php } ?>
-                                </div>
-                                <div class="bt">
-                                    <a href=""><button>예약변경</button></a>
-                                </div>
-                            </li>
+                            <div class = "grid-width">
+                                <li>학생정보
+                                    <div class = "infor">
+                                        <?php foreach($rows as $row) { ?>
+                                            <p>이름: <?= $row["user_id"] ?></p>
+                                            <?php if($row["student_id"] == null) {?> <p>등록된 학번이 없습니다.</p>
+                                            <?php } else {?> <p>학번: <?= $row["student_id"] ?></p> <?php } ?>
+                                            <?php if($row["student_id"] == null) {?> <p>등록된 학과가 없습니다.</p>
+                                            <?php } else {?> <p>학과: <?= $row["department"] ?></p> <?php } ?>
+                                        <?php } ?>
+                                    </div>
+                                    <div class="bt">
+                                        <a href="modify_info.php"><button>회원정보수정</button></a>
+                                    <div>
+                                </li>
+                            </div> 
+                            <div class = "grid-width">
+                                <li>예약확인
+                                    <div class = "infor">
+                                        <?php $query = "SELECT * FROM reservation WHERE id='$user_id'";
+                                            $rows = $db->query($query);
+                                            if($rows->rowCount() == 0) {
+                                        ?>
+                                            <p>예약이 없습니다.</p>
+                                        <?php
+                                            } else {
+                                                foreach($rows as $row) {
+                                        ?>
+                                            <p>방번호: <?= $row["reserve_room_no"] ?></p>
+                                            <p>예약날짜: <?= $row["reserve_date"] ?></p>
+                                            <p>예약시간: <?= $row["reserve_time"] ?></p>
+                                        <?php   } ?>
+                                        <?php } ?>
+                                    </div>
+                                    <div class="bt">
+                                        <a href=""><button>예약변경</button></a>
+                                    </div>
+                                </li>
+                            </div>    
                         </ul>
                     </div>
                     <?php
