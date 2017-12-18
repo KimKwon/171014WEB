1fec22a6d13f1671daea350f5907fe2b55a9cf11
baesh3744 <baesh3744@naver.com>
2017-12-18 22:52:05 +0900

    Update delete_reservation.php

--- html/delete_reservation.php
+++ html/delete_reservation.php
@@ -1,15 +1,16 @@
 <?php
     session_start();
-    if(!empty($_POST)) {
         try {
-            $id = $_SESSION["id"];
-            $db = new PDO("mysql:host=gs-db-instance1.cgkevqnkktap.ap-northeast-2.rds.amazonaws.com;port=3306;dbname=smash","smash","smash1219");
-    		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            $reserved_date = $_POST["reservation"];
-            foreach($reserved_date as $date){
-                $db->exec("DELETE FROM reservation where id='$id' AND reserve_date='$date'");
+            if(!empty($_POST)) {
+                $id = $_SESSION["id"];
+                $db = new PDO("mysql:host=gs-db-instance1.cgkevqnkktap.ap-northeast-2.rds.amazonaws.com;port=3306;dbname=smash","smash","smash1219");
+                $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $reserved_date = $_POST["reservation"];
+                foreach($reserved_date as $date){
+                    $db->exec("DELETE FROM reservation where id='$id' AND reserve_date='$date'");
+                }
             }
 
             // echo("<script>location.replace('mypage.php');</script>");
@@ -17,5 +18,4 @@
         } catch(PDOException $e) {
             echo $e->getMessage();
         }
-    }
 ?>
